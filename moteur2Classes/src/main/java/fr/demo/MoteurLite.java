bd44b2a1294be5fa126043e4e499685e74feb666
Sandesh Patil <99490622+sandepat@users.noreply.github.com>
2023-10-16 15:55:58 +0200

    Update MoteurLite.java
    1. workflowdb_common and workflowdb_plugin plugins added
    2. minimal workflow to fill-in/update the database in moteurLite

--- moteur2Classes/src/main/java/fr/demo/MoteurLite.java
+++ moteur2Classes/src/main/java/fr/demo/MoteurLite.java
@@ -1,13 +1,19 @@
 package fr.insalyon.creatis.moteurlite;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.System.Logger;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import javax.sound.sampled.Port;
 
@@ -27,7 +33,15 @@ import fr.insalyon.creatis.gasw.execution.GaswOutputParser;
 import fr.insalyon.creatis.gasw.execution.GaswStatus;
 import fr.insalyon.creatis.gasw.parser.GaswParser;
 import fr.insalyon.creatis.gasw.plugin.ListenerPlugin;
+import fr.insalyon.creatis.moteur.plugins.workflowsdb.WorkflowsDBListener;
+import fr.insalyon.creatis.moteur.plugins.workflowsdb.bean.Workflow;
+import fr.insalyon.creatis.moteur.plugins.workflowsdb.bean.WorkflowStatus;
+import fr.insalyon.creatis.moteur.plugins.workflowsdb.dao.WorkflowDAO;
+import fr.insalyon.creatis.moteur.plugins.workflowsdb.dao.WorkflowsDBDAOException;
+import fr.insalyon.creatis.moteur.plugins.workflowsdb.dao.WorkflowsDBDAOFactory;
 import javassist.bytecode.Descriptor.Iterator;
+
+
 
 public class MoteurLite {
     private boolean stop = false;
@@ -39,9 +53,11 @@ public class MoteurLite {
         new MoteurLite(args);
 
     }
+
     public MoteurLite(String[] args) throws GaswException, SAXException, IOException, URISyntaxException {
         String gaswFilePath = args[0];
         String inputsFilePath = args[1];
+        String workflowId = String.valueOf(new Random().nextInt());
         GaswParser gaswParser = new GaswParser();
         gaswParser.parse(gaswFilePath);
         Map<String, String> inputsMap = new DataSetParser(inputsFilePath).getInputValues();
@@ -50,14 +66,44 @@ public class MoteurLite {
         gasw.setNotificationClient(gaswMonitor);
         gaswMonitor.start();
         String jobID = gasw.submit(gaswInput);
+        createWorkflow(workflowId);
         System.out.println("job launched : " + jobID);
         System.out.println("Waiting for Notification");
     }
+
+    public void createWorkflow(String workflowId) {
+
+
+        try {
+            WorkflowsDBDAOFactory workflowsDBDAOFactory = new WorkflowsDBDAOFactory();
+            WorkflowDAO workflowDAO = workflowsDBDAOFactory.getWorkflowDAO();
+
+            Workflow workflow = new Workflow();
+            workflow.setId(workflowId);
+            workflow.setUsername("sandesh");
+            workflow.setStatus(WorkflowStatus.Queued);
+            workflow.setStartedTime(new Date());
+            
+
+
+            System.out.println("adding workflow in workflowsdb : " + workflowId);
+            workflowDAO.add(workflow);
+            System.out.println("added workflow in workflowsdb : " + workflowId);
+
+        } catch (WorkflowsDBDAOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
     public class GaswMonitor extends Thread {     
         public GaswMonitor() {}
+        
         public void run() {        
             labelx:
             while(!MoteurLite.this.stop) {
+                System.out.println("test");
+                
                 synchronized(this) {
                     try {
                         this.wait();
@@ -66,9 +112,12 @@ public class MoteurLite {
                     }
                 }
             java.util.Iterator<GaswOutput> i$ = MoteurLite.this.gasw.getFinishedJobs().iterator();
+            System.out.println(gasw.getFinishedJobs());
             GaswOutput gaswOutput;
 
-                System.out.println(i$);
+            System.out.println(i$);
+            //System.out.println(i$.next());  
+            
                 if (!i$.hasNext()) {
                     MoteurLite.this.gasw.waitForNotification();
                     continue labelx;
@@ -76,6 +125,8 @@ public class MoteurLite {
             gaswOutput = (GaswOutput)i$.next();
             
             System.out.println("Status : " + gaswOutput.getExitCode());
+            
+            System.out.println("Output Path : " + gaswOutput.getUploadedResults());
             MoteurLite.this.stop = true;
             }
             try {
@@ -84,6 +135,6 @@ public class MoteurLite {
                 System.out.println("Exiting");
             } catch (GaswException e) {
                 e.printStackTrace();
-            }}}
-            
+            }
+        }}
 }
